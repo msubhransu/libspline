9c3c3380de2e11a60c07f7863fe181a2c645ae98
Subhransu Maji <subhransu.maji@gmail.com>
2014-05-26 11:25:57 -0500

    copyrights

--- matlabModel.h
+++ matlabModel.h
@@ -1,3 +1,9 @@
+/* Author : Subhransu Maji
+ *
+ * Matlab functions wrapper
+ *
+ * Version 1.0
+ */
 #ifndef _ADDITIVE_MODEL_MATLAB_H
 #define _ADDITIVE_MODEL_MATLAB_H
 const char *model_to_matlab_structure(mxArray *plhs[], additiveModel *model_);
